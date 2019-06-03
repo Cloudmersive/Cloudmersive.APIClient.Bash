2a800558804e3bd75dd0510e2313a0051996c192
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-06-02 18:19:30 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 Remove-Item -path ./client -recurse
 & java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l bash -o client -c packageconfig.json
 (Get-Content ./client/cloudmersive-cli).replace('host=""', 'host="https://api.cloudmersive.com"') | Set-Content ./client/cloudmersive-cli
-
+(Get-Content ./client/cloudmersive-cli) -replace "if [[ -z `"$host`" ]]; then*ERROR_MSG=`"ERROR: No hostname provided!!! You have to  provide on command line option '--host ...'`"*exit 1*fi", "" | Set-Content ./client/cloudmersive-cli
 
 Copy-Item ./client/README.md ./README.md
