4e99295d18e7050b2bec5a33196c238b1e17332b
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-07-27 19:22:11 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 Remove-Item -path ./client -recurse
-& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l bash -o client -c packageconfig.json
+& java -jar swagger-codegen-cli-2.4.14.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l bash -o client -c packageconfig.json
 (Get-Content ./client/cloudmersive-cli).replace('host=""', 'host="https://api.cloudmersive.com"') | Set-Content ./client/cloudmersive-cli
 (Get-Content ./client/cloudmersive-cli).replace('header_content_type=""', 'header_content_type="application/json"') | Set-Content ./client/cloudmersive-cli
 (Get-Content ./client/cloudmersive-cli).replace("if [[ -z `"$host`" ]]; then", "if 0; then") | Set-Content ./client/cloudmersive-cli
